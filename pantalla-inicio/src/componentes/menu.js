478b840d785b6960c4e6760c6fefc913fa26b404
Jose Mateo Bernal <133398097+MateoBernal06@users.noreply.github.com>
2024-08-19 15:57:35 -0500

    tercera version

--- pantalla-inicio/src/componentes/menu.js
+++ pantalla-inicio/src/componentes/menu.js
@@ -10,10 +10,10 @@ function Menu(){
                 <img className="imagen_logo" src={Logo}></img>
             </div>
             <div className="boton_a">
-                <a href="https://registro-delta.vercel.app/"><button className='boton' id='boton_registro' type='subtime'>Registro</button></a>
+                <button className='boton' id='boton_registro' type='subtime'>Registro</button>
             </div>
             <div className="boton_b">
-                <a href="https://interfaces-login-mt.vercel.app/"> <button className='boton' id='boton_iniciar' type='subtime'>Iniciar sesion</button></a>
+                <button className='boton' id='boton_iniciar' type='subtime'>Iniciar sesion</button>
             </div>
             <div className="color_b">5</div>
         </div>
